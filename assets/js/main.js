3b5cb165cc3cdb893fc59462a437bca3cf9beb53
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-08-31 12:51:34 +0100

    Added the view counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,4 +259,13 @@
    */
   new PureCounter();
 
+  // JS code for view count
+  const counter = document.querySelector(".counter-number");
+  async function updateCounter() {
+    let response = await fetch("https://atkgtnia75eexurlni6ct63h2q0dwsqb.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+  }
+  updateCounter();
+
 })()
