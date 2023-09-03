be2bfe1db0a377c61b917f14905909c2f925dcb1
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-09-03 03:55:40 +0100

    upadte the IaC

---
+++ IaC-Terraform/lambda/lambdaFunc.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+awsService = boto3.resource('dynamodb')
+table = awsService.Table('cloud-resume')
+
+def lambda_handler(event, context):
+    # TODO implement
+    response = table.get_item(Key={
+        'id':'1'
+    })
+    views = response['Item']['views']
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={
+        'id':'1',  
+        'views':views
+    })
+    return views
