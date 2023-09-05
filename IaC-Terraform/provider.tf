ae7b651d5a2545b6bf97ccc08b28d23d5a3376e9
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-09-05 22:23:18 +0100

    Update the region in provider file

--- IaC-Terraform/provider.tf
+++ IaC-Terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-2"
+  region                   = "us-east-1"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "cloudResumeProfile"
 }
